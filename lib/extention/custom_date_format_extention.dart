eff0f9534776af238a2f5d1413baad3f26c93f11
Alief Mumtaz <mumtazalieff@gmail.com>
2022-07-11 16:06:50 +0800

    fix - syntax date format from switch case to for loop

--- lib/extention/custom_date_format_extention.dart
+++ lib/extention/custom_date_format_extention.dart
@@ -3,34 +3,29 @@ extension DateFormat on String {
     String date = split('-')[2];
     String month = split('-')[1];
 
-    switch (month) {
-      case '01':
-        return '$date Jan';
-      case '02':
-        return '$date Feb';
-      case '03':
-        return '$date Mar';
-      case '04':
-        return '$date Apr';
-      case '05':
-        return '$date May';
-      case '06':
-        return '$date Jun';
-      case '07':
-        return '$date Jul';
-      case '08':
-        return '$date Aug';
-      case '09':
-        return '$date Sep';
-      case '10':
-        return '$date Oct';
-      case '11':
-        return '$date Nov';
-      case '12':
-        return '$date Des';
-      default:
+    List<String> months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Des'
+    ];
 
-      return '';
+    String dateFormat = '';
+
+    for (var i = 1; i <= 12; i++) {
+      if (month == '0$i' || month == '12') {
+        dateFormat = '${months[i - 1]} $date';
+      } 
     }
+
+    return dateFormat;
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:volantis_weather_app/main.dart';
